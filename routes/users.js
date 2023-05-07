e10f9917bcf927f1f5f441d09daf20f50ddd4a44
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-05-07 13:44:18 -0700

    Set up routes for user routes;

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,34 @@
+const express = require('express');
+const User = require('../models/user');
+const router = new express.Router();
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
-
+router.get('/', async (req, res, next) => {
+  try {
+    let users = await User.all();
+    return res.json({users});
+  } catch(e) {
+    return next(e)
+  }
+})
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
+router.get('/:username', async (req, res, next) => {
+  try {
+    let user = await User.get(req.params.username);
+    return res.json({user})
+  } catch(e) {
+    return next(e)
+  }
+})
 
 /** GET /:username/to - get messages to user
  *
@@ -21,7 +39,14 @@
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-
+router.get('/:username/to', async(req, res, next) => {
+  try {
+    let messages = await User.messagesTo(req.params.username);
+    return res.json({messages})
+  } catch(e) {
+    return next(e)
+  }
+})
 
 /** GET /:username/from - get messages from user
  *
@@ -32,3 +57,14 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get('/:username/from', async(req, res, next) => {
+  try {
+    let messages = await User.messagesFrom(req.params.username);
+    return res.json({messages})
+  } catch(e) {
+    return next(e)
+  }
+})
+
+
+module.exports = router;
